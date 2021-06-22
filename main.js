71d7f8015bd537f25b4ee4770cc349e2da01fc7f
vanderson <vandersonec@gmail.com>
2021-06-22 19:17:09 -0300

    Finalizaçao do menu e sombreamento

--- main.js
+++ main.js
@@ -16,3 +16,14 @@ for(const link of links){
     nav.classList.remove('show')
   })
 }
+
+/* Mudar o sombreamento em scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+window.addEventListener('scroll', function(){
+  if (window.scrollY >= navHeight){
+    header.classList.add('scroll')
+  }else{
+    header.classList.remove('scroll')
+  }
+})
